0e7183213278848532cbb270146d080da7525dc9
Miriam Lang <miriam.lang@student.reutlingen-university.de>
2020-06-23 07:46:18 +0200

    main created

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	PM5CTL0 &= ~LOCKLPM5;
+    P5DIR |= BIT2;              // Put P5.2
+
+
+
+   for(;;){
+       volatile unsigned int i;
+
+       P5OUT ^= BIT2;
+
+       i = 10000;
+       do i--;
+       while(i != 0);
+
+   }
+}
